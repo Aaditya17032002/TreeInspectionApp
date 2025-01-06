acd0a2f1f9689690bf4522ea4b5eb363688bc42c
Aditya Jangam <adityajangam19@gmail.com>
2025-01-06 16:16:19 +0530

    initialCommit 14 06-01-25 (19)

--- components/map/components/new-inspection-dialog.tsx
+++ components/map/components/new-inspection-dialog.tsx
@@ -215,7 +215,7 @@ export function NewInspectionDialog({ open, onOpenChange, onSave }: NewInspectio
         }}
       >
         <DialogContent className=" bg-white fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-0 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full">
-          <DialogHeader className="p-4 pb-2">
+          <DialogHeader className="bg-white p-4 pb-2">
             <DialogTitle>New Inspection</DialogTitle>
           </DialogHeader>
           <form onSubmit={handleSubmit} className="space-y-4 p-4 pt-2 max-h-[80vh] overflow-y-auto">
